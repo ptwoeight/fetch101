a73bb44c37cf414da625530987357ee200e19b7b
ptwoeight <109756794+ptwoeight@users.noreply.github.com>
2025-02-18 16:53:09 +0000

    completed unit tests!

--- index.js
+++ index.js
@@ -12,3 +12,50 @@ async function showMessage(elem, url) {
   const content = await response.text();
   elem.textContent = content;
 }
+
+
+async function showList(elem, url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  data.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    elem.appendChild(li);
+  });
+}
+
+
+function startShowingMessage(elem, url) {
+  setInterval(async function() {
+    const response = await fetch(url);
+    const content = await response.text();
+    elem.textContent = content;
+  }, 1000);
+}
+
+
+async function handleError(elem, url) {  
+  const response = await fetch(url);
+  const content = await response.text();
+
+  if (!response.ok) {
+    elem.textContent = 'OH DEAR';
+  } 
+  else {
+    elem.textContent = content;
+  }
+}
+
+
+async function drawBox(canvas, url) {
+  const canvasContext = canvas.getContext('2d');
+
+  setInterval(async function() {
+    const response = await fetch(url);
+    const content = await response.json();
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(content.x, content.y, 10, 10);
+  }, 1000);
+}
